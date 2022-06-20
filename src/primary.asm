03cb6f9da6b1e47c304fcdc38ca20e650519905f
Malcom Wu <malcomwu@gmail.com>
2022-06-21 03:21:08 +0800

    Create primary.asm

---
+++ src/primary.asm
@@ -0,0 +1,40 @@
+# Primary
+This is a test for ideas.
+
+## Icon
+```tex
+\pi = 3.14159265433(1)
+\epsilon = 2.7(1)
+\sqrt(2) = 1.414(1)
+```
+
+## Introduction
+Thanks to the father of TeX and his Arts of computer languages
+by macro and knowlege. I learn from him a lot. If anyone found
+a mistake, I will pay any money what you want. It is possible
+that I cannot pay you. But I will pay you.
+
+
+## Description
+(omitted (?))
+
+This is to describ the first-in and first-out (FIFO) machine in
+finite automata. This is for the customer's visit and revisit
+of goods in a local store.
+
+
+PREAMBLE
+[||]_/Direction\_[||]
+
+FIRST
+MEMO 1. a + b = c  ||
+Add a and b to it(register).
+MEMO 2. It becomes a stack with two componets of a and b.  ||
+Increace the register to perform c.
+Push c to a new stack with a and b. 
+
+NEXT
+Pop b from the stack of c.
+
+LAST
+(Our atmosphere is good. For your surrondings..)
